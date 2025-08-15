031f83b82c050e9cf8f281afda5e2d7f5a775f55
sangeetha <Artemis6161@github.com>
2025-08-15 14:56:16 +0530

    added link to SocialsSidebsr

--- src/components/SocialSidebar.jsx
+++ src/components/SocialSidebar.jsx
@@ -13,17 +13,17 @@ const SocialSidebar = () => {
       {/* List of social icons */}
       <ul className="flex flex-col items-center">
         <li className="mb-4">
-          <a href="https://linkedin.com/in/your-username" target="_blank" rel="noopener noreferrer" className="text-text-secondary hover:text-accent hover:scale-110 transition-all duration-300">
+          <a href="www.linkedin.com/in/sangeetha-rajan-283106198" target="_blank" rel="noopener noreferrer" className="text-text-secondary hover:text-accent hover:scale-110 transition-all duration-300">
             <FaLinkedin size={24} />
           </a>
         </li>
         <li className="mb-4">
-          <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer" className="text-text-secondary hover:text-accent hover:scale-110 transition-all duration-300">
+          <a href="https://github.com/Artemis6161" target="_blank" rel="noopener noreferrer" className="text-text-secondary hover:text-accent hover:scale-110 transition-all duration-300">
             <FaGithub size={24} />
           </a>
         </li>
         <li>
-          <a href="mailto:your.email@example.com" className="text-text-secondary hover:text-accent hover:scale-110 transition-all duration-300">
+          <a href="mailto:sanrajan2@gmail.com" className="text-text-secondary hover:text-accent hover:scale-110 transition-all duration-300">
             <HiOutlineMail size={24} />
           </a>
         </li>
